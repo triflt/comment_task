1319045e5b420c0839f1dc7266ffa080029fb272
triflt <89809140+triflt@users.noreply.github.com>
2023-02-13 22:58:34 +0300

    all works very well and i believe i`ll deploy it...

--- comment_task/reviews/generate.py
+++ comment_task/reviews/generate.py
@@ -2,27 +2,26 @@ import os
 import re
 
 import numpy as np
-import pandas as pd
 import pickle
 
 from tqdm import tqdm
-from catboost import CatBoostRegressor
 from gensim.models import Word2Vec
 from gensim.utils import tokenize
 
-import sklearn
-import torch
-import nltk
+
+model_wv = Word2Vec.load('../models/trained_word2vec.model')
+
+model_reg = pickle.load(open('../models/cb_reg.model', "rb"))
 
 
-def sent2vec_generate(question: str, model):
-    dim = model.vector_size
+def sent2vec_generate(question: str):
+    dim = model_wv.vector_size
     token_question = np.array(list(tokenize(question, lowercase=True, deacc=True)))
     question_array = np.zeros(dim)
     count = 0
     for word in token_question:
-        if model.wv.__contains__(str(word)):
-            question_array += (np.array(model.wv[str(word)]))
+        if model_wv.wv.__contains__(str(word)):
+            question_array += (np.array(model_wv.wv[str(word)]))
             count += 1
     if count == 0:
         return question_array
@@ -30,8 +29,8 @@ def sent2vec_generate(question: str, model):
     return question_array / count
 
 
-def rating_predict(vector, model):
-    predict = model.predict(vector)
+def rating_predict(vector):
+    predict = model_reg.predict(vector)
 
     '''def remake(x):
         flag = -1 if x < 5 else 1
@@ -47,14 +46,9 @@ def rating_predict(vector, model):
 
 
 def emotion_predict(rating):
-    return 'pos' if rating >= 6 else 'neg'
+    return 'positive :)' if rating >= 6 else 'negative :('
 
-
-model_wv = Word2Vec.load('models/trained_word2vec.model')
-
-model_reg = pickle.load(open('models/cb_reg.model', "rb"))
-
-vec = sent2vec_generate('I was prepared for a turgid talky soap opera cum travelogue, '
+"""vec = sent2vec_generate('I was prepared for a turgid talky soap opera cum travelogue, '
                         'but was pleased to find a fast-paced script, an underlying moral,'
                         ' excellent portrayals from all the actors, especially Peter Finch,'
                         ' amazing special effects, suspense, and beautiful cinematography--'
@@ -63,7 +57,7 @@ vec = sent2vec_generate('I was prepared for a turgid talky soap opera cum travel
                         ' before she gave in to the gaspy hysterics that marred her later work.'
                         ' All the supporting players round it out, and I do wonder who trained all those elephants.', model_wv)
 
-rating = rating_predict(vec, model_reg)
+rating = rating_predict(vec)
 emotion = emotion_predict(rating)
 print(rating)
-print(emotion)
+print(emotion)"""
--- comment_task/reviews/views.py
+++ comment_task/reviews/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from .models import Article
 from .forms import ArticleForm
+from .generate import sent2vec_generate, rating_predict, emotion_predict
 
 
 def home_reviews(request):
@@ -13,8 +14,13 @@ def create(request):
     if request.method == 'POST':
         form = ArticleForm(request.POST)
         if form.is_valid():
+            rev = form.cleaned_data['review']
+            vec = sent2vec_generate(rev)
+            rating = round(rating_predict(vec), 1)
+            emotion = emotion_predict(rating)
+            form.cleaned_data.update({'emotion_predict': emotion, 'rating_predict': rating})
             form.save()
-            return redirect('home')
+            return render(request, 'reviews/article_detail.html', form.cleaned_data)
         else:
             error = 'Invalid input'
 
